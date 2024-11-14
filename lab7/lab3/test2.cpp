a46eaaa68934d5591f32f91a8edb51f1a28851bf
Ju-wan <rd002@naver.com>
2024-11-14 15:20:56 +0900

    chat_server

---
+++ lab7/lab3/test2.cpp
@@ -0,0 +1,18 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int s=socket(AF_INET, SOCK_DGRAM,IPPROTO_UDP);
+    cout<< "Socekt ID: "<<s<<endl;
+    close(s);
+    
+    s=socket(AF_INET, SOCK_DGRAM,IPPROTO_UDP);
+    cout<< "Socekt ID: "<<s<<endl;
+    close(s);
+    return 0;
+}
